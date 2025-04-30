1ffec29c9ad37633eae97dfde3c84f5482a41813
KolmakovKirill <130784185+KolmakovKirill@users.noreply.github.com>
2025-04-30 22:38:49 +0500

    Update models.py

--- myproject/parks/models.py
+++ myproject/parks/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userdata')
     ssid = models.CharField(max_length=100, blank=True)
-    data = models.JSONField(default=dict, blank=True)  # здесь позже будет состояние ламп/датчиков
+    data = models.JSONField(default=dict, blank=True)  
 
     def __str__(self):
         return f"Data for {self.user.username}"
