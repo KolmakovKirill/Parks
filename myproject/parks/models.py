50f528a6b1e3e95cc70e2715770f9ca94de0d8d3
KolmakovKirill <kkolmakov075@gmail.com>
2025-05-15 01:35:00 +0500

    web

--- myproject/parks/models.py
+++ myproject/parks/models.py
@@ -9,11 +9,19 @@ class UserData(models.Model):
     def __str__(self):
         return f"Data for {self.user.username}"
 
+class ESP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ssid = models.CharField(max_length=50)
+    ip_address = models.GenericIPAddressField(protocol='IPv4')  # Добавлено поле IP
+
+    def __str__(self):
+        return f"{self.ssid} ({self.ip_address})"
 
 class LightZone(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     zone_number = models.PositiveSmallIntegerField()
     is_on = models.BooleanField(default=False)
+    esp = models.ForeignKey(ESP, on_delete=models.CASCADE, default=0)  # Привязка к ESP
 
     class Meta:
         unique_together = ('user', 'zone_number')
@@ -29,3 +37,4 @@ class SystemLog(models.Model):
 
     def __str__(self):
         return f"{self.timestamp}: Пользователь {self.user.username} -> Зона {self.zone_number} [{self.action}]"
+
